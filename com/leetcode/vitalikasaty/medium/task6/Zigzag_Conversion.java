03b9f72294497c061fb0c840d017dfea8042e57d
Vitali Kasaty <vitalik007007@gmail.com>
2022-07-26 20:45:05 +0300

    Task6 - done, tests passed

--- com/leetcode/vitalikasaty/medium/task6/Zigzag_Conversion.java
+++ com/leetcode/vitalikasaty/medium/task6/Zigzag_Conversion.java
@@ -1,19 +1,17 @@
 package com.leetcode.vitalikasaty.medium.task6;
 
+import java.io.LineNumberReader;
+
 /*
 The string "PAYPALISHIRING" is written in a zigzag pattern on a given number 
 of rows like this: (you may want to display this pattern in a fixed font for better legibility)
-
 P   A   H   N
 A P L S I I G
 Y   I   R
-
 And then read line by line: "PAHNAPLSIIGYIR"
-
 Write the code that will take a string and make this conversion given a number of rows:
 string convert(string s, int numRows);
  
-
 Example 1:
 Input: s = "PAYPALISHIRING", numRows = 3
 Output: "PAHNAPLSIIGYIR"
@@ -26,11 +24,9 @@ P     I    N
 A   L S  I G
 Y A   H R
 P     I
-
 Example 3:
 Input: s = "A", numRows = 1
 Output: "A"
-
 Constraints:
 	1 <= s.length <= 1000
 	s consists of English letters (lower-case and upper-case), ',' and '.'.
@@ -41,7 +37,7 @@ public class Zigzag_Conversion {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		System.out.println(solution.convert("PAYPALISHIRING", 3));
+		System.out.println(solution.convert("a", 2));
 	}
 
 }
@@ -49,7 +45,26 @@ public class Zigzag_Conversion {
 class Solution {
 	public String convert(String s, int numRows) {
 
-		int numColumns = s.length();
+		if (numRows == 1) {
+			return s;
+		}
+
+		int numColumns = 0;
+		int test = s.length();
+
+		while (test > 0) {
+			test -= numRows;
+			numColumns++;
+
+			if (test > 0 && (test - (numRows - 2)) > 0) {
+				numColumns += numRows - 2;
+				test -= numRows - 2;
+			} else if (test > 0) {
+				numColumns += test;
+				test -= test;
+			}
+		}
+
 		int numSymbol = 0;
 		String[][] zigZag = new String[numRows][numColumns];
 		String result = "";
@@ -61,11 +76,10 @@ class Solution {
 					break;
 				}
 
-				if (j % numRows == 0) {
+				if (j % (numRows - 1) == 0) {
 					zigZag[i][j] = Character.toString(s.charAt(numSymbol));
 					numSymbol++;
-				} else if ((i + j) % numRows == 0 && i != numRows - 1) {
-
+				} else if ((i + j) % (numRows - 1) == 0) {
 					zigZag[i][j] = Character.toString(s.charAt(numSymbol));
 					numSymbol++;
 				} else {
@@ -79,12 +93,14 @@ class Solution {
 			for (int j = 0; j < numColumns; j++) {
 
 				if (zigZag[i][j] != null) {
-					result += zigZag[i][j];
+					if (!zigZag[i][j].equals(" ")) {
+						result += zigZag[i][j];
+					}
+
 				}
 
 			}
 		}
-		result = result.replace(" ", "");
 		return result;
 	}
 }
