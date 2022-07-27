5e5fe12aee670e6eeaa911206b8586e8a9e0012a
Vitali Kasaty <vitalik007007@gmail.com>
2022-07-27 22:31:57 +0300

    Task1403 start

---
+++ com/leetcode/vitalikasaty/easy/task1403/Minimum_Subsequence_in_Non_Increasing_Order.java
@@ -0,0 +1,63 @@
+package com.leetcode.vitalikasaty.easy.task1403;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/*
+Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence. 
+If there are multiple solutions, return the subsequence with minimum size and if there still exist multiple solutions, 
+return the subsequence with the maximum total sum of all its elements. A subsequence of an array can be obtained by erasing some (possibly zero) elements from the array. 
+Note that the solution with the given constraints is guaranteed to be unique. 
+Also return the answer sorted in non-increasing order.
+
+Example 1:
+Input: nums = [4,3,10,9,8]
+Output: [10,9] 
+Explanation: The subsequences [10,9] and [10,8] are minimal such that the sum of their elements is strictly greater than the sum of elements not included, however, the subsequence [10,9] has the maximum total sum of its elements. 
+
+Example 2:
+Input: nums = [4,4,7,6,7]
+Output: [7,7,6] 
+Explanation: The subsequence [7,7] has the sum of its elements equal to 14 which is not strictly greater than the sum of elements not included (14 = 4 + 4 + 6). Therefore, the subsequence [7,6,7] is the minimal satisfying the conditions. Note the subsequence has to returned in non-decreasing order.  
+
+Example 3:
+Input: nums = [6]
+Output: [6]
+*/
+
+public class Minimum_Subsequence_in_Non_Increasing_Order {
+	
+	public static void main(String[] args) {
+		
+    	List<Integer> result = new ArrayList<>();
+    	
+    	int[] nums = new int[] {1, 5, -10, 154, 14, 54};
+
+    	int max = Arrays.stream(nums).max().getAsInt();
+    	System.out.println(max);
+    	
+    	System.out.println(result);
+    	
+		
+//		Solution solution = new Solution();
+//		System.out.println(solution.minSubsequence(null));
+
+	}
+
+}
+
+class Solution {
+    public List<Integer> minSubsequence(int[] nums) {
+        
+    	List<Integer> result = new ArrayList<>();
+    	
+
+    	return result;
+    }
+}
