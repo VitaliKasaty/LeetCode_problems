6514bbb747014e939460c231e63fe754bc29028b
Vitali Kasaty <vitalik007007@gmail.com>
2022-08-24 20:53:36 +0300

    Task1185 - Done

--- com/leetcode/vitalikasaty/easy/task1185/Day_of_the_Week.java
+++ com/leetcode/vitalikasaty/easy/task1185/Day_of_the_Week.java
@@ -1,5 +1,9 @@
 package com.leetcode.vitalikasaty.easy.task1185;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 /*
 Given a date, return the corresponding day of the week for that date.
 The input is given as three integers representing the day, month and year respectively.
@@ -23,10 +27,17 @@ The given dates are valid dates between the years 1971 and 2100.
 
 public class Day_of_the_Week {
 
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.dayOfTheWeek(31, 8, 2019));
+	}
+
 }
 
 class Solution {
-    public String dayOfTheWeek(int day, int month, int year) {
-        
-    }
+	public String dayOfTheWeek(int day, int month, int year) {
+		Calendar calendar = new GregorianCalendar(year, month - 1, day);
+		String result = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US);
+		return result;
+	}
 }
