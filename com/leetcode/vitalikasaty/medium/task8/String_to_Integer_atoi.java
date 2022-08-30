0c98691a010481715f301e6c3d28f3e4277f9d51
Vitali Kasaty <vitalik007007@gmail.com>
2022-08-30 20:45:31 +0300

    Start task8

---
+++ com/leetcode/vitalikasaty/medium/task8/String_to_Integer_atoi.java
@@ -0,0 +1,19 @@
+package com.leetcode.vitalikasaty.medium.task8;
+
+public class String_to_Integer_atoi {
+	
+	public static void main(String[] args) {
+		
+		Solution solution = new Solution();
+		System.out.println(solution.myAtoi("test"));
+
+	}
+
+}
+
+class Solution {
+    public int myAtoi(String s) {
+        
+    	return 1;
+    }
+}
