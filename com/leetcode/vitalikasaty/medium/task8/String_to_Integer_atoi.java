3a0f465b726e39e0458f539cb49ea473378c8448
Vitali Kasaty <vitalik007007@gmail.com>
2022-08-30 23:20:41 +0300

    Task8 - done

--- com/leetcode/vitalikasaty/medium/task8/String_to_Integer_atoi.java
+++ com/leetcode/vitalikasaty/medium/task8/String_to_Integer_atoi.java
@@ -1,19 +1,123 @@
 package com.leetcode.vitalikasaty.medium.task8;
 
+/*
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+The algorithm for myAtoi(string s) is as follows:
+	1. Read in and ignore any leading whitespace.
+	2. Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either.
+	This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+	3. Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+	4. Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0.
+	Change the sign as necessary (from step 2).
+	5. If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range.
+	Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+	6. Return the integer as the final result.
+
+Note:
+Only the space character ' ' is considered a whitespace character.
+Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+ 
+
+Example 1:
+Input: s = "42"
+Output: 42
+Explanation: The underlined characters are what is read in, the caret is the current reader position.
+Step 1: "42" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "42" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "42" ("42" is read in)
+           ^
+The parsed integer is 42.
+Since 42 is in the range [-231, 231 - 1], the final result is 42.
+
+Example 2:
+Input: s = "   -42"
+Output: -42
+Explanation:
+Step 1: "   -42" (leading whitespace is read and ignored)
+            ^
+Step 2: "   -42" ('-' is read, so the result should be negative)
+             ^
+Step 3: "   -42" ("42" is read in)
+               ^
+The parsed integer is -42.
+Since -42 is in the range [-231, 231 - 1], the final result is -42.
+
+Example 3:
+Input: s = "4193 with words"
+Output: 4193
+Explanation:
+Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+             ^
+The parsed integer is 4193.
+Since 4193 is in the range [-231, 231 - 1], the final result is 4193.
+ 
+
+Constraints:
+0 <= s.length <= 200
+s consists of English letters (lower-case and upper-case), digits (0-9), ' ', '+', '-', and '.'.
+*/
+
 public class String_to_Integer_atoi {
-	
+
 	public static void main(String[] args) {
-		
 		Solution solution = new Solution();
-		System.out.println(solution.myAtoi("test"));
-
+		System.out.println(solution.myAtoi("   642req"));
 	}
-
 }
 
 class Solution {
-    public int myAtoi(String s) {
-        
-    	return 1;
-    }
+	public int myAtoi(String s) {
+
+		String resultString = "";
+		int plusMinus = 1;
+		int startPosNumberInString = 0;
+		int answer = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '-') {
+				plusMinus = -1;
+				startPosNumberInString = i + 1;
+				break;
+			} else if (s.charAt(i) == '+') {
+				startPosNumberInString = i + 1;
+				break;
+			} else if (s.charAt(i) >= 48 && s.charAt(i) <= 57) {
+				startPosNumberInString = i;
+				break;
+			} else if (s.charAt(i) != ' ') {
+				return 0;
+			}
+		}
+
+		for (int i = startPosNumberInString; i < s.length(); i++) {
+			if (s.charAt(i) >= 48 && s.charAt(i) <= 57) {
+				resultString += s.charAt(i);
+			} else {
+				break;
+			}
+		}
+
+		if (resultString.length() == 0) {
+			return 0;
+		}
+
+		try {
+			answer = Integer.parseInt(resultString) * plusMinus;
+		} catch (NumberFormatException ex) {
+			if (plusMinus == 1) {
+				answer = Integer.MAX_VALUE;
+			} else {
+				answer = Integer.MIN_VALUE;
+			}
+		}
+
+		return answer;
+	}
 }
