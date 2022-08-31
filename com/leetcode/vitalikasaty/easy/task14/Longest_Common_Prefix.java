19f56bcf03daaed50b457dc6e01511bc7f6b4808
Vitali Kasaty <vitalik007007@gmail.com>
2022-08-31 21:13:13 +0300

    Task14 - done

--- com/leetcode/vitalikasaty/easy/task14/Longest_Common_Prefix.java
+++ com/leetcode/vitalikasaty/easy/task14/Longest_Common_Prefix.java
@@ -1,5 +1,9 @@
 package com.leetcode.vitalikasaty.easy.task14;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 /*
  * Write a function to find the longest common prefix string amongst an array of strings.
 If there is no common prefix, return an empty string "". 
@@ -20,11 +24,35 @@ strs[i] consists of only lowercase English letters.
 */
 
 public class Longest_Common_Prefix {
-
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.longestCommonPrefix(new String[] { "flower", "flow", "flight" }));
+	}
 }
 
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        
-    }
+	public String longestCommonPrefix(String[] strs) {
+
+		StringBuilder result = new StringBuilder("");
+		boolean finished = false;
+
+		Comparator<String> comprator = (o1, o2) -> o1.length() - o2.length();
+		Arrays.sort(strs, comprator);
+
+		for (int i = 0; i < strs[0].length() && !finished; i++) {
+			char symbol = strs[0].charAt(i);
+
+			for (int j = 1; j < strs.length; j++) {
+				if (strs[j].charAt(i) != symbol) {
+					finished = true;
+					break;
+				}
+			}
+			if (!finished) {
+				result.append(symbol);
+			}
+		}
+
+		return result.toString();
+	}
 }
