30ce000464e04a182df286f4ffb7fdb4768e2fc4
Vitali Kasaty <vitalik007007@gmail.com>
2022-09-08 20:12:44 +0300

    Start task5(medium)

---
+++ com/leetcode/vitalikasaty/medium/task5/Longest_Palindromic_Substring.java
@@ -0,0 +1,33 @@
+package com.leetcode.vitalikasaty.medium.task5;
+
+/*
+Given a string s, return the longest palindromic substring in s.		 
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+
+Constraints:
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+*/
+
+public class Longest_Palindromic_Substring {
+	
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.longestPalindrome("babad"));
+	}
+}
+
+class Solution {
+    public String longestPalindrome(String s) {
+        
+    	return "s";
+    }
+}
