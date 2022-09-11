4b8fd6709671be199660354e691e047c8391bbd0
Vitali Kasaty <vitalik007007@gmail.com>
2022-09-11 20:25:14 +0300

    Task16, solution for not big input array

---
+++ com/leetcode/vitalikasaty/medium/task16/Three_Sum_Closest.java
@@ -0,0 +1,59 @@
+package com.leetcode.vitalikasaty.medium.task16;
+
+/*
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+Return the sum of the three integers.
+You may assume that each input would have exactly one solution. 
+
+Example 1:
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+Example 2:
+Input: nums = [0,0,0], target = 1
+Output: 0
+
+Constraints:
+3 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+*/
+
+public class Three_Sum_Closest {
+	public static void main(String[] args) {
+		
+
+
+		Solution solution = new Solution();
+		System.out.println(solution.threeSumClosest(new int[] { -1, 2, 1, -4 }, 1));
+
+	}
+}
+
+class Solution {
+	public int threeSumClosest(int[] nums, int target) {
+
+		int sumThree = nums[0] + nums[1] + nums[2];
+		int currentDistance = Math.abs(target - sumThree);
+
+		for (int i = 0; i < nums.length - 2; i++) {
+			for (int j = i + 1; j < nums.length - 1; j++) {
+				for (int k = j + 1; k < nums.length; k++) {
+
+					int tempSumThree = nums[i] + nums[j] + nums[k];
+					int tempDistance = Math.abs(target - tempSumThree);
+
+					if (tempDistance < currentDistance) {
+						sumThree = tempSumThree;
+						currentDistance = tempDistance;
+					}
+
+				}
+			}
+		}
+
+		return sumThree;
+
+	}
+}
