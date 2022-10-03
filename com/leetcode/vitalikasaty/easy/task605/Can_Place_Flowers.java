f97f5120c415e6e34475ca51ea5805e4a2f35909
Vitali Kasaty <vitalik007007@gmail.com>
2022-10-03 22:30:39 +0300

    Task 605 - done

--- com/leetcode/vitalikasaty/easy/task605/Can_Place_Flowers.java
+++ com/leetcode/vitalikasaty/easy/task605/Can_Place_Flowers.java
@@ -1,7 +1,5 @@
 package com.leetcode.vitalikasaty.easy.task605;
 
-import javax.net.ssl.SSLEngine;
-
 /*
  You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
 Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty,
@@ -25,41 +23,41 @@ There are no two adjacent flowers in flowerbed.
 public class Can_Place_Flowers {
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		System.out.println(solution.canPlaceFlowers(new int[] { 0, 0, 0 }, 1));
+		System.out.println(solution.canPlaceFlowers(new int[] { 0 }, 0));
 	}
 }
 
 class Solution {
 	public boolean canPlaceFlowers(int[] flowerbed, int n) {
 
-		if (n == 0) {
-			return true;
-		}
-		if (flowerbed.length == 1 && flowerbed[0] == 0) {
-			return true;
-		}
-
-		for (int i = 0; i < flowerbed.length - 1; i++) {
-
+		for (int i = 0; i < flowerbed.length - 1 && n != 0; i++) {
 			if (flowerbed[i] == 0) {
 				if (flowerbed[i + 1] == 0) {
+					flowerbed[i] = 1;
+					i++;
 					n--;
 				}
-				i++;
 			} else {
 				i++;
 			}
-			if (i == flowerbed.length - 2 && n != 0) {
-				if (flowerbed[flowerbed.length - 2] == 0 && flowerbed[flowerbed.length - 1] == 0) {
-					n--;
-				}
-			}
-
 			if (n == 0) {
-				return true;
+				break;
 			}
 
 		}
-		return false;
+
+		if (flowerbed.length > 2 && flowerbed[flowerbed.length - 1] == 0 && flowerbed[flowerbed.length - 2] == 0
+				&& n != 0) {
+			n--;
+		} else if (flowerbed.length == 1 && flowerbed[0] == 0 && n != 0) {
+			n--;
+		}
+
+		if (n == 0) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 }
