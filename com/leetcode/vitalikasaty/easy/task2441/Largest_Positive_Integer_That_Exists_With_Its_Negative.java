4041dc1f4b2023e225471dd0c01f9b19e8852871
Vitali Kasaty <vitalik007007@gmail.com>
2022-10-25 18:57:43 +0300

    Task2441 - done

---
+++ com/leetcode/vitalikasaty/easy/task2441/Largest_Positive_Integer_That_Exists_With_Its_Negative.java
@@ -0,0 +1,55 @@
+package com.leetcode.vitalikasaty.easy.task2441;
+
+import java.util.Arrays;
+
+/*
+Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+Return the positive integer k. If there is no such integer, return -1. 
+
+Example 1:
+Input: nums = [-1,2,-3,3]
+Output: 3
+Explanation: 3 is the only valid k we can find in the array.
+
+Example 2:
+Input: nums = [-1,10,6,7,-7,1]
+Output: 7
+Explanation: Both 1 and 7 have their corresponding negative values in the array. 7 has a larger value.
+
+Example 3:
+Input: nums = [-10,8,6,7,-2,-3]
+Output: -1
+Explanation: There is no a single valid k, we return -1.
+
+Constraints:
+1 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+nums[i] != 0
+*/
+
+public class Largest_Positive_Integer_That_Exists_With_Its_Negative {
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.findMaxK(new int[] { -1, 10, 6, 7, -7, 1 }));
+	}
+}
+
+class Solution {
+	public int findMaxK(int[] nums) {
+
+		Arrays.sort(nums);
+		System.out.println(Arrays.toString(nums));
+
+		for (int i = nums.length - 1; i > 0; i--) {
+			if (nums[i] > 0) {
+				int needNegativeNum = nums[i] * -1;
+				if (Arrays.stream(nums).anyMatch(x -> x == needNegativeNum)) {
+					return nums[i];
+				}
+			} else {
+				break;
+			}
+		}
+		return -1;
+	}
+}
