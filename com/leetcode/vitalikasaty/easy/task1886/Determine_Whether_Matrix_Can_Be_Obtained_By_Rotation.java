452b053590221b3927817debc9be8a8beebaf68d
Vitali Kasaty <vitalik007007@gmail.com>
2022-11-19 16:28:26 +0300

    Task1886 - done

--- com/leetcode/vitalikasaty/easy/task1886/Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation.java
+++ com/leetcode/vitalikasaty/easy/task1886/Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation.java
@@ -1,5 +1,7 @@
 package com.leetcode.vitalikasaty.easy.task1886;
 
+import java.util.Arrays;
+
 /*
 Given two n x n binary matrices mat and target, 
 return true if it is possible to make mat equal to target by rotating mat in 90-degree increments, or false otherwise.
@@ -23,59 +25,40 @@ Explanation: We can rotate mat 90 degrees clockwise two times to make mat equal 
 public class Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation {
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		//System.out.println(solution.findRotation(new int[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 1, 1, 1 } },
-		//		new int[][] { { 1, 1, 1 }, { 0, 1, 0 }, { 0, 0, 0 } }));	
-		System.out.println(solution.findRotation(new int[][] { { 0, 1}, { 1, 1 } },	new int[][] { { 1, 0 }, { 0, 1}}));
+		System.out.println(solution.findRotation(new int[][] { { 0, 0, 0 }, { 0, 0, 1 }, { 0, 0, 1 } },
+				new int[][] { { 0, 0, 0 }, { 0, 0, 1 }, { 0, 0, 1 } }));
 	}
 }
 
 class Solution {
 	public boolean findRotation(int[][] mat, int[][] target) {
 
-		if (canRotate90(mat, target)) {
+		if (Arrays.deepEquals(mat, target)) {
 			return true;
-		} else if (canRotate180(mat, target)) {
-			return true;
-		} else if (canRotate270(mat, target)) {
-			return true;
-		} else return false;
-	}
+		}
 
-	public boolean canRotate90(int[][] mat, int[][] target) {
-		int n = target.length - 1;
-
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= n; j++) {
-				if (target[i][j] != mat[n - j][i]) {
-					return false;
-				}
+		for (int i = 1; i < 4; i++) {
+			mat = rotate90(mat);
+			if (Arrays.deepEquals(mat, target)) {
+				return true;
 			}
 		}
-		return true;
+
+		return false;
 	}
 
-	public boolean canRotate180(int[][] mat, int[][] target) {
-		int n = mat.length - 1;
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= n; j++) {
-				if (target[i][j] != mat[n - i][n - j]) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+	public int[][] rotate90(int[][] mat) {
 
-	public boolean canRotate270(int[][] mat, int[][] target) {
-		int n = mat.length - 1;
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= n; j++) {
-				if (target[i][j] != mat[j][n - i]) {
-					return false;
-				}
+		int n = mat.length;
+		int[][] result = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				result[i][j] = mat[n - j - 1][i];
+
 			}
 		}
-		return true;
+		return result;
 	}
 
 }
